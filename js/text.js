524403f9700080c226ba4baa749a5fd478fa7bcb
aki0921 <66670058+aki0921@users.noreply.github.com>
2022-04-05 19:19:52 +0900

    Add files via upload

---
+++ js/text.js
@@ -0,0 +1,25 @@
+const text = baffle('.uuidTitle');
+	text.set({
+		characters : 'zpxzVpasdfh86136░█▒ ░░░█▓ >░░ ▓/▒█▓ █ █>█▒sayg▒ ░░░█▓ >yf',
+		speed: 100
+	});
+	text.start();
+	text.reveal(80000);
+
+	const text1 = baffle('.uuidFull');
+	text1.set({
+		characters : 'qwertyuiopasdfgh8613611888zpxzVpasdfh86136░█▒ ░░░█▓ >░░ ▓/▒█▓ █ █>█▒sayg▒ ░░░█▓ >yf',
+		speed: 120
+	});
+	text1.start();
+	text1.reveal(5500);
+
+	/* Copyright (c) 2022 by MIMAXUZ (https://codepen.io/MIMAXUZ/pen/PgwjyB)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
